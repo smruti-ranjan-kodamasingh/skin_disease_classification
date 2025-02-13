e1769bddcd82db8d14c980f9d506ae6e8656d5ca
smruti-ranjan-kodamasingh <kodamasinghsmrutiranjan2003@gmail.com>
2025-02-13 17:12:16 +0000

    data ingested

---
+++ main.py
@@ -0,0 +1,13 @@
+from skin_disease_classifier import logger
+from skin_disease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
