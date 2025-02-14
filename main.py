cdc939c4bfd872bb13ee6bc2ee210a363baf5beb
smruti-ranjan-kodamasingh <kodamasinghsmrutiranjan2003@gmail.com>
2025-02-14 18:03:59 +0000

    base model prepared

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from skin_disease_classifier import logger
 from skin_disease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from skin_disease_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -11,3 +13,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
